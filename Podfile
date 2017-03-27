abc13417d16382abadfcd6711265da1d72a160a7
Charles Chandler <charles.chandler@willowtreeapps.com>
2017-03-27 19:25:19 -0400

    Update pods

--- Podfile
+++ Podfile
@@ -11,4 +11,5 @@ target :LiveActionBetrayal do
 	pod 'TableDiff', :git => 'https://github.com/willowtreeapps/tablediff', :tag => 'v0.5'
 	pod 'PinkyPromise', :git => 'https://github.com/willowtreeapps/PinkyPromise.git', :branch => 'swift-3'
 	pod 'Spruce'
+	pod 'CircleMenu', '~> 2.0.0'
 end
--- Pods/Target Support Files/Pods-LiveActionBetrayal/Pods-LiveActionBetrayal-frameworks.sh
+++ Pods/Target Support Files/Pods-LiveActionBetrayal/Pods-LiveActionBetrayal-frameworks.sh
@@ -84,12 +84,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/CircleMenu/CircleMenu.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PinkyPromise/PinkyPromise.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReSwift/ReSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Spruce/Spruce.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TableDiff/TableDiff.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/CircleMenu/CircleMenu.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PinkyPromise/PinkyPromise.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReSwift/ReSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Spruce/Spruce.framework"

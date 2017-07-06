7014fac9f4d8c2cd5886ea1a9c66fee198e76451
Charles Chandler <charles.chandler@willowtreeapps.com>
2017-07-06 13:58:07 -0400

    Add haunt assets

--- Code/LiveActionBetrayal/WatcherHauntViewController.swift
+++ Code/LiveActionBetrayal/WatcherHauntViewController.swift
@@ -7,8 +7,15 @@
 //
 
 import UIKit
+import ReSwift
 
 final class WatcherHauntViewController: BaseViewController {
+    
+    @IBOutlet weak var startHauntButton: BlurButton! {
+        didSet {
+            startHauntButton.setTitle("HAUNT HAS BEGUN!", for: .disabled)
+        }
+    }
     
     @IBAction func startHauntButtonTapped(_ sender: BlurButton) {
         HauntController.triggerHaunt()
@@ -22,6 +29,18 @@ extension WatcherHauntViewController: WatcherType {
         super.viewDidLoad()
         
         setupView()
+        
+        AppStore.shared.subscribe(self)
+    }
+    
+}
+
+extension WatcherHauntViewController: StoreSubscriber {
+    
+    func newState(state: AppState) {
+        if state.hauntTriggered && startHauntButton.isEnabled {
+            startHauntButton.isEnabled = false
+        }
     }
     
 }

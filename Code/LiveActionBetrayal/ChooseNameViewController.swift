135737ad06e56bd734a12803d7c0e54f20df95e6
Charles Chandler <charles.chandler@willowtreeapps.com>
2017-07-20 17:05:22 -0400

    Add validation for the name field.

--- Code/LiveActionBetrayal/ChooseNameViewController.swift
+++ Code/LiveActionBetrayal/ChooseNameViewController.swift
@@ -8,24 +8,41 @@
 
 import UIKit
 import Spruce
+import PinkyPromise
 
 final class ChooseNameViewController: BaseViewController {
     
     @IBOutlet weak var blurView: UIVisualEffectView!
     @IBOutlet weak var nameEntry: UITextField!
+    @IBOutlet weak var warningLabel: UILabel!
     
-    func validateName(name: String?) -> Bool {
-        guard let name = name else { return false }
+    func validateName(name: String?) -> Result<Void> {
+        guard let name = name, !name.isEmpty else {
+            return .failure(ValidationFailure.empty)
+        }
         
-        guard name.lowercased() != "watcher" else { return false }
+        guard name != "watcher" else {
+            return .failure(ValidationFailure.watcher)
+        }
+        
+        guard name.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted) == nil else {
+            return .failure(ValidationFailure.invalidCharacter)
+        }
         
         for player in AppStore.shared.state.connectedPlayers {
             if player.name == name {
-                return false
+                return .failure(ValidationFailure.taken)
             }
         }
         
-        return true
+        return .success()
+    }
+    
+    enum ValidationFailure: Error {
+        case empty
+        case watcher
+        case invalidCharacter
+        case taken
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -65,18 +82,39 @@ extension ChooseNameViewController: MainMenuType {
 extension ChooseNameViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if validateName(name: textField.text) {
+        let result = validateName(name: textField.text)
+        
+        switch result {
+        case .success():
             textField.resignFirstResponder()
             performSegue(withIdentifier: IDs.Segues.NameToAttributes.rawValue, sender: nil)
             nameEntry.backgroundColor = .clear
+            
             return true
-        } else {
-            UIView.animate(withDuration: 0.3, animations: { 
+            
+        case .failure(let error as ValidationFailure):
+            switch error {
+            case .empty:
+                warningLabel.text = "Name cannot be empty."
+            case .watcher:
+                warningLabel.text = "Your fate is not to be the watcher this night."
+            case .invalidCharacter:
+                warningLabel.text = "Names cannot contain spaces or special characters."
+            case .taken:
+                warningLabel.text = "That name is already taken."
+            }
+            
+            UIView.animate(withDuration: 0.3, animations: {
                 self.nameEntry.backgroundColor = .red
                 self.nameEntry.text = ""
                 self.nameEntry.placeholder = "Please choose another."
+                self.warningLabel.alpha = 1.0
             })
+            
             return false
+            
+        case .failure(_):
+            fatalError()
         }
     }
     

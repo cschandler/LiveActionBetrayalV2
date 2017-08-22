290520e5328d7929052b4540f0f91311041d61df
Charles Chandler <charles.chandler@willowtreeapps.com>
2017-08-22 11:05:33 -0400

    Style haunt buttons.

--- Code/LiveActionBetrayal/HauntPickerButtonCell.swift
+++ Code/LiveActionBetrayal/HauntPickerButtonCell.swift
@@ -10,19 +10,22 @@ import UIKit
 
 final class HauntPickerButtonCell: UITableViewCell, ClassNameNibLoadable {
     
-    @IBOutlet weak var rejectButton: BlurButton!
-    @IBOutlet weak var startButton: BlurButton!
+    @IBOutlet weak var rejectButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
     
     var rejectButtonTapped: SimpleClosure?
     var startButtonTapped: SimpleClosure?
+    
+    private let tint = UIColor(red: 0.07, green: 0.39, blue: 0.55, alpha: 1.0)
+    private let background = UIColor(red: 0.02, green: 0.1, blue: 0.15, alpha: 1.0)
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         contentView.backgroundColor = .clear
         
-        rejectButton.setTitle("Reject", for: .normal)
-        startButton.setTitle("Start!", for: .normal)
+        setup(button: rejectButton, withTitle: "reject")
+        setup(button: startButton, withTitle: "start!")
     }
     
     @IBAction func rejectButtonTapped(_ sender: BlurButton) {
@@ -33,4 +36,13 @@ final class HauntPickerButtonCell: UITableViewCell, ClassNameNibLoadable {
         startButtonTapped?()
     }
     
+    private func setup(button: UIButton, withTitle title: String) {
+        button.setTitle(title.uppercased(), for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: button.titleLabel?.font.pointSize ?? 17.0)
+        button.layer.cornerRadius = 4.0
+        button.layer.borderWidth = 2.0
+        button.layer.borderColor = tint.cgColor
+        button.tintColor = tint
+        button.backgroundColor = background
+    }
 }
